7e2546fce2734c9ad97679586efc76f348744b2b
aunirudrad <anu15-4170@diu.edu.bd>
2025-10-14 13:48:36 +0600

    form data submission added

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express'); //express import
 
+const cors = require('cors');
+
 const app = express(); //express app create
 
 const PORT = 5000; //server port
@@ -7,11 +9,28 @@ const PORT = 5000; //server port
 //to read JSON data
 
 app.use(express.json());
+app.use(cors());
 
 //test route
 app.get('/', (req, res) => {
     res.send('backend is working');
 } );
+
+app.post('/api/appointments', (req, res) => {
+    const {
+        name, email, subject, message
+    } = req.body;
+
+    if(!name || !email || !subject || !message){
+        return res.status(400).json({success: false, error: 'All fields are required!'});
+    }
+    console.log('Appointment received', req.body);
+
+    res.status(200).json({
+        success: true,
+        message: 'Appointment received successfully',
+    });
+});
 
 //server on
 
